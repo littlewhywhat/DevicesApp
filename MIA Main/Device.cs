a93ca1c95c74471a5a0937f590eb40bbf625790a
Roman Vayvod <littlewhywhat@gmail.com>
2013-12-14 22:41:42 +0400

    Start developing

---
+++ MIA Main/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIA_Main
+{
+    class Device
+    {
+        public int Id { get; private set; }
+        public string Info { get; private set; }
+        public Device(int id, string info)
+        {
+            Id = id;
+            Info = info;
+        }
+    }
+}
