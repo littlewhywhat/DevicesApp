57e422973dc42a8726ea007d04916b0236be0ed6
Roman Vayvod <littlewhywhat@gmail.com>
2013-12-15 00:46:11 +0400

    DBHelper created

---
+++ MIA Main/DBHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MIA_Main
+{
+    static class DBHelper
+    {
+        public static SqlConnection Connection { get; private set; }
+        public static void SetConnection(SqlConnectionStringBuilder builder)
+        {
+            Connection = new SqlConnection(builder.ConnectionString);
+
+        }
+        private static void GetData()
+        {
+            string commandText = GetDeviceInfoCommand();
+            
+            SqlCommand command = new SqlCommand(commandText, Connection);
+            var reader = command.ExecuteReader();
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                reader.GetValue(i);
+
+            }
+                
+        }
+
+        private static string GetDeviceInfoCommand()
+        {
+            return "SELECT * FROM Devices";
+        }
+    }
+}
