bbd0d4b4cdbee9f3139fde35bb53bf58cbcd8ba3
Roman Vayvod <littlewhywhat@gmail.com>
2013-12-23 23:11:20 +0400

    creating GetTimestamp, UpdateClient

---
+++ MIA Main/DBAction/GetTimestamp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace MiaMain
+{
+    public class GetTimestamp : DBAction
+    {
+        
+        protected override object Act(SqlConnection connection)
+        {
+            return new SqlCommand(GetCommandText(), connection).ExecuteScalar();
+        }
+
+        private string GetCommandText()
+        {
+            return "SELECT MAX(Timestamp) FROM Logging";
+        }
+    }
+}
---
+++ MIA Main/UpdateClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace MiaMain
+{
+    public static class UpdateClient
+    {
+        static byte[] timestamp;
+        public static void Do(SqlConnection connection)
+        {
+            var tempTimestamp = new GetTimestamp().Perform(connection);
+            if (tempTimestamp != timestamp)
+            {
+                
+            }
+        }
+    }
+}
